5e29e1102f2390aac6864d2d73474c73d24d2687
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-13 20:23:26 +0000

    Дописал методы в структуру ноты

--- core/note.h
+++ core/note.h
@@ -3,17 +3,44 @@
 
 struct note{
     public:
-        int octave; // 1-субконтр, 2-контр, 3-большая, 4-малая, 5-первая, 6-вторая,..
-        int note_name; // 
-        char sign;
+        int octave; // 1 - субконтр, 2 - контр, 3 - большая,..
+        char name; // a,b,c,d,e,f,g - наименование нот (по ascii a = 97, b = 98 и тд.)
+        float height; // высота ноты от до первой октавы
+        char sign; // "+" = диез, "-" = бемоль,  "~" = отсутствие знака
 
         int getHeight(int height){
             if (sign == '~'){
-                return height;
+                return name - 96; // 1 - нота a
             } else if(sign == '+'){
-                return ++height;
+                return (height - 0.5) - 96;
             } else if(sign == '-'){
-                return --height;
+                return (height - 0.5) - 96;
+            }
+        }
+
+        void change(int index){
+            if(index = 0){
+                return;
+            }
+            if(index > 0){
+                for (int x=0; x<index; ++x){
+                    if(name < 103){
+                        ++name;
+                    } else {
+                        name = 97;
+                        ++octave;
+                    }
+                }
+            }
+            if(index < 0){
+                for (int x=0; x>index; ++index){
+                    if(name > 97){
+                        --name;
+                    } else {
+                        name = 103;
+                        --octave;
+                    }
+                }
             }
         }
         
