ca4f80ec97b4d5d49fe96059beb6ca525aaa8a6e
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-14 21:51:56 +0000

    Еще немного модификаций

--- core/note.h
+++ core/note.h
@@ -5,7 +5,11 @@ struct note{
         int octave;  // 1 - субконтр, 2 - контр, 3 - большая,..
         char name;   // a,b,c,d,e,f,g - наименование нот (по ascii a = 97, b = 98 и тд.)
 
-        int getTone(int n, char key){
+        int getTone(char key){
+            char n = name;
+            if(key < 97 || key > 103){
+                return -1;
+            }
             n -= 96;
             for(int x=0; x<n; ++x){
                 key++;
@@ -15,31 +19,15 @@ struct note{
             }
             return 7 - (key - 96);
         }
-        int getTone(char key){
-            return getTone(name, key);
-        }
 
         void changeNote(int index){
-            if(index > 0){
-                for (int x=0; x<index; ++x){
-                    if(name < 103){
-                        ++name;
-                    } else {
-                        name = 97;
-                        ++octave;
-                    }
-                }
-            }
-            if(index < 0){
-                for (int x=0; x>index; ++index){
-                    if(name > 97){
-                        --name;
-                    } else {
-                        name = 103;
-                        --octave;
-                    }
-                }
-            }
+            if(name + index > 103 || name + index < 97){return;}
+            name += index;
+        }
+
+        void changeOctave(int index){
+            if (name + index < 0) {return;}
+            octave += index;
         }
     private:
 };
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Данный бот будет использовать библиотеку tgBot.cpp
+// Данный бот будет использовать библиотеку tgBot.cpp
 // Инструкция по установке: https://github.com/reo7sp/tgbot-cpp 
 // Инструкция по библиотеке: http://reo7sp.github.io/tgbot-cpp/
 #include <iostream>
@@ -6,12 +6,14 @@
 int main() {
     note n;
     n.octave = 5;
-    n.name = 'c';
+    n.name = 'd';
 
-    //n.changeNote(-1);
+    n.changeNote(-1);     //  d --> c
+    n.changeOctave(-1);   //  5 --> 4
 
     std::cout << "октава: " << n.octave << std::endl;
     std::cout << "нота: " << n.name << std::endl;
-    std::cout << "Высота в тональности " << 'g' << ": " << n.getTone('g') << std::endl;
+    std::cout << "Высота в тональности " << 'c' << ": " << n.getTone('c') << std::endl;
+    std::cout << "Высота в тональности " << 'a' << ": " << n.getTone('a') << std::endl;
     return 0;
 }
