dae84809e3320ab1bd8d86a1d88bb62a1c3b1d28
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-18 07:46:45 +0300

    Update musicScale.h

--- core/musicScale.h
+++ core/musicScale.h
@@ -1,12 +1,12 @@
 #pragma once
 
 struct musicScale {
-    int camertone = 440;             // в этом строю проще, тк он нацело делится на 12
+    int camertone = 440;             // частота ноты ля
     float nextToneKey = 1,0594732;   // корень 12 степени из 2
 
     char getToneName(Tone t){
         for(int x=0; x<12; ++x){
-
+            
         }
     }
 };
